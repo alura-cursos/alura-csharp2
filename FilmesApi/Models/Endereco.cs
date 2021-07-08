f3205817cbcb2c1ab2a40e3cb022a841082ed89f
Daniel Artine <daniel.mastub@stoneage.com.br>
2021-07-07 22:40:33 -0300

    aula-2

--- FilmesApi/Models/Endereco.cs
+++ FilmesApi/Models/Endereco.cs
@@ -16,6 +16,6 @@ namespace FilmesApi.Models
         public string Logradouro { get; set; }
         public string Bairro { get; set; }
         public int Numero { get; set; }
-        public Cinema Cinema { get; set; }
+        public virtual Cinema Cinema { get; set; }
     }
 }
